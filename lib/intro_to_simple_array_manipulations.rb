e31c244fe3a8a5ce35fd756507e886d59373a13d
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-08 17:05:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,7 @@ def using_push (array, string)
   colors_in_the_rainbow = ["red","orange","yellow","green","blue","indigo"]
   next_color = colors_in_the_rainbow.push("violet")
 end
+
+def using_unshift (array, string)
+  
+end
